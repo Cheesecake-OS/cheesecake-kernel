ba961b260fcf16581ad445514bf6bc83c049706b
Martico2432 <mail.joks02@gmail.com>
2026-06-23 15:24:56 +0200

    Generate vdi hard drives

--- make_image/src/main.rs
+++ make_image/src/main.rs
@@ -1,4 +1,5 @@
 use std::path::Path;
+use std::process::Command;
 
 fn main() {
     let profile = std::env::var("PROFILE").unwrap_or_else(|_| "debug".into());
@@ -13,5 +14,39 @@ fn main() {
     let mut boot = bootloader::UefiBoot::new(kernel_path);
     boot.create_disk_image(output_path).unwrap();
     println!("Disk image made at: {:?}", output_path);
-    println!("Profile: {}", profile)
+    println!("Profile: {}", profile);
+
+    if profile == "release" {
+        println!("Making vdi file");
+        let output_vdi_text = format!(
+            "target/x86_64-unknown-none/{}/cheesecake_kernel.vdi",
+            profile
+        );
+        let vdi_path = Path::new(&output_vdi_text);
+
+        if vdi_path.exists() {
+            let _ = std::fs::remove_file(vdi_path);
+        }
+        let status = Command::new("C:\\Program Files\\Oracle\\VirtualBox\\VBoxManage.exe")
+            .arg("convertfromraw")
+            .arg(output_path) // Ruta del archivo .img de entrada
+            .arg(vdi_path) // Ruta del archivo .vdi de salida
+            .arg("--format")
+            .arg("VDI")
+            .status();
+        match status {
+            Ok(s) if s.success() => {
+                println!("VDI image successfully made at: {:?}", vdi_path);
+            }
+            Ok(s) => {
+                eprintln!("VBoxManage exited with an error status: {}", s);
+            }
+            Err(e) => {
+                eprintln!(
+                            "Failed to execute VBoxManage. Is VirtualBox installed in the default path? Error: {}",
+                            e
+                        );
+            }
+        }
+    }
 }
