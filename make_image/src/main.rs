d61ea89a1671d7178cf045c938110a5cf5152fe9
Martico2432 <mail.joks02@gmail.com>
2026-06-23 15:03:28 +0200

    Make release imager

--- make_image/src/main.rs
+++ make_image/src/main.rs
@@ -1,9 +1,17 @@
 use std::path::Path;
 
 fn main() {
-    let kernel_path = Path::new("target/x86_64-unknown-none/debug/cheesecake_kernel");
-    let output_path = Path::new("target/x86_64-unknown-none/debug/cheesecake_kernel.img");
+    let profile = std::env::var("PROFILE").unwrap_or_else(|_| "debug".into());
+
+    let output_kernel_path = &format!("target/x86_64-unknown-none/{}/cheesecake_kernel", profile);
+    let kernel_path = Path::new(output_kernel_path);
+    let output_formated_text = &format!(
+        "target/x86_64-unknown-none/{}/cheesecake_kernel.img",
+        profile
+    );
+    let output_path = Path::new(output_formated_text);
     let mut boot = bootloader::UefiBoot::new(kernel_path);
     boot.create_disk_image(output_path).unwrap();
     println!("Disk image made at: {:?}", output_path);
+    println!("Profile: {}", profile)
 }
