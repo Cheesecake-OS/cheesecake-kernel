ef9057b14e5c81eea9df1615bfea3b5f1915be22
Martico2432 <mail.joks02@gmail.com>
2026-06-23 15:03:51 +0200

    Forgot .bat files

---
+++ dev.bat
@@ -0,0 +1,27 @@
+@echo off
+setlocal
+set QEMU=qemu-system-x86_64
+set BIN=target\x86_64-unknown-none\debug\cheesecake_kernel.img
+set OVMF_CODE=D:\qemu\share\edk2-x86_64-code.fd
+set OVMF_VARS=D:\qemu\share\edk2-i386-vars.fd
+
+cd cheesecake_kernel
+cargo build --target x86_64-unknown-none || exit /b 1
+cd ..
+
+cargo run -p make_image || exit /b 1
+
+%QEMU% ^
+  -drive if=pflash,format=raw,readonly=on,file="%OVMF_CODE%" ^
+  -drive if=pflash,format=raw,file="%OVMF_VARS%" ^
+  -drive format=raw,file=%BIN% ^
+  -serial stdio -m 128M -no-reboot -no-shutdown ^
+  -accel whpx,kernel-irqchip=off 2>nul || ^
+%QEMU% ^
+  -drive if=pflash,format=raw,readonly=on,file="%OVMF_CODE%" ^
+  -drive if=pflash,format=raw,file="%OVMF_VARS%" ^
+  -drive format=raw,file=%BIN% ^
+  -serial stdio -m 128M -no-reboot -no-shutdown ^
+  -accel tcg
+
+endlocal
